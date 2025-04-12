45ad67a3cf4ef1aafa04c5e2356243e2e4950454
Jonathan Elsner <jeelsner@outlook.com>
2025-04-11 17:21:50 -0700

    Neovim compatibility settings for tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,12 +47,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	end,
 })
 
+-- https://ast-grep.github.io/guide/tools/editors.html#neovim
 require('lspconfig').ast_grep.setup({
-  -- these are the default options, you only need to specify
-  -- options you'd like to change from the default
-  cmd = { 'ast-grep', 'lsp' },
-  filetypes = { "c", "cpp", "rust", "go", "java", "python", "javascript", "typescript", "html", "css", "kotlin", "dart", "lua" },
-  root_dir = require('lspconfig.util').root_pattern('sgconfig.yaml', 'sgconfig.yml')
+	cmd = {"ast-grep", "lsp"},
+	filetypes = { "c", "cpp", "rust", "go", "java", "python", "javascript", "typescript", "html", "css", "kotlin", "dart", "lua" },
+	root_dir = require('lspconfig.util').root_pattern('sgconfig.yaml', 'sgconfig.yml')
 })
 
 require('mason').setup({})
