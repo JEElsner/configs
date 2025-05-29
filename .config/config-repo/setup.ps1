324c2cacb651fe5a6b00db5da0b783a32f773574
Jonathan Elsner <jeelsner@outlook.com>
2025-05-29 16:03:21 -0700

    Add debug info to setup script

--- .config/config-repo/setup.ps1
+++ .config/config-repo/setup.ps1
@@ -3,5 +3,8 @@
 #
 # you will probably have to run this as administrator
 
+# Print commands as they are run
+Set-PSDebug -Trace 1
+
 # nvim config files
 New-Item -ItemType SymbolicLink -Path $env:localappdata\nvim -Target $HOME\.config\nvim
